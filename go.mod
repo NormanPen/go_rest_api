871858ab47797f28d89dd3d2992e029446dd9adb
Norman Pendzich <Norman.Pendzich@damago.eu>
2023-08-08 14:50:49 +0200

    asd

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/NormanPen/go_rest_api
 
 go 1.20
 
-require (
-	github.com/NormanPen/l298nhbridge v0.0.0-20230808121146-824c11067c6f // indirect
-	github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
-)
+require github.com/stianeikeland/go-rpio/v4 v4.6.0
+
+require github.com/NormanPen/l298nhbridge v0.0.0-20230808123123-d3a49c539a8f // indirect
--- main.go
+++ main.go
@@ -2,26 +2,18 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/NormanPen/l298nhbridge"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, Gopher!")
 
 	// Initialisieren der Motoren und GPIO-Pins
 	l298nhbridge.Init()
 
 	// Motoren vorwärts bewegen
 	l298nhbridge.SetMotors(0.5, 0.5) // Hier kannst du die Geschwindigkeiten einstellen
-	l298nhbridge.SetMotors(0.5, 0.5)
-	l298nhbridge.SetMotors(0.5, 0.5)
-	l298nhbridge.SetMotors(0.5, 0.5)
-	l298nhbridge.SetMotors(0.5, 0.5)
-	l298nhbridge.SetMotors(0.5, 0.5)
-	// Warte eine Weile, während die Motoren laufen
-	// Hier kannst du deine eigene Logik einfügen
-	// z.B.: time.Sleep(time.Second * 5)
+	// ...
 
 	// Stoppe die Motoren
 	l298nhbridge.StopMotors()
